177eccb19b8dc0431582e2519ac7f3f9206c842c
Alexander <sworddanser>
2017-07-16 19:14:23 +0700

    HW rework

--- 2.4-external-programs/programm.py
+++ 2.4-external-programs/programm.py
@@ -2,10 +2,23 @@ import subprocess
 import os.path
 import os
 
-file_path = os.path.abspath(os.path.dirname(__file__))
-image = os.listdir(os.path.join(file_path, 'Source'))
-for i in image:
-	subprocess.run('convert .\Source\*.jpg -resize 200 .\Results\image.jpg')
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
 
-  	
-	 
+
+
+def create_dir():
+    os.chdir(FILE_PATH)
+    if os.path.exists(os.path.join(FILE_PATH, 'Results')) != True:
+        os.mkdir('Results', 0o777)
+
+def convert():
+    image = os.listdir(os.path.join(FILE_PATH, 'Source'))
+    for i in image:
+        subprocess.run('convert .\Source\%s -resize 200 .\Results\%s' % (i, i) )
+
+def main():
+    create_dir()
+    convert()
+
+
+main()
